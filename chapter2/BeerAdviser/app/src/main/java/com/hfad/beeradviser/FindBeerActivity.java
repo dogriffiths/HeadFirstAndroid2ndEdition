4a8c5df9e3ba76f3b2cbac3466adcdef6b0fc7fb
David Griffiths <dogriffiths@gmail.com>
2017-03-13 12:55:11 +0000

    feat: Add on click method for "Find beer" button
    Also fallback to Activity instead of AppCompatActivity.

--- chapter2/BeerAdviser/app/src/main/java/com/hfad/beeradviser/FindBeerActivity.java
+++ chapter2/BeerAdviser/app/src/main/java/com/hfad/beeradviser/FindBeerActivity.java
@@ -1,13 +1,31 @@
 package com.hfad.beeradviser;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Spinner;
+import android.widget.TextView;
 
-public class FindBeerActivity extends AppCompatActivity {
+public class FindBeerActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_beer);
+    }
+
+    //Call when the user clicks the button
+    public void onClickFindBeer(View view) {
+        //Get a reference to the TextView
+        TextView brands = (TextView) findViewById(R.id.brands);
+
+        //Get a reference to the Spinner
+        Spinner color = (Spinner) findViewById(R.id.color);
+
+        //Get the selected item in the Spinner
+        String beerType = String.valueOf(color.getSelectedItem());
+
+        //Display the selected item
+        brands.setText(beerType);
     }
 }
