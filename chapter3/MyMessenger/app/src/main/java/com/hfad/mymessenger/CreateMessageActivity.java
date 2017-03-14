c0d67170d8ac87ef640b1247fdaf757e22ff0d25
David Griffiths <dogriffiths@gmail.com>
2017-03-14 13:29:20 +0000

    feat: Get first activity to start second

--- chapter3/MyMessenger/app/src/main/java/com/hfad/mymessenger/CreateMessageActivity.java
+++ chapter3/MyMessenger/app/src/main/java/com/hfad/mymessenger/CreateMessageActivity.java
@@ -1,7 +1,9 @@
 package com.hfad.mymessenger;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class CreateMessageActivity extends Activity {
 
@@ -9,5 +11,11 @@ public class CreateMessageActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_message);
+    }
+
+    // Call onSendMessage() when the button is clicked
+    public void onSendMessage(View view) {
+        Intent intent = new Intent(this, ReceiveMessageActivity.class);
+        startActivity(intent);
     }
 }
