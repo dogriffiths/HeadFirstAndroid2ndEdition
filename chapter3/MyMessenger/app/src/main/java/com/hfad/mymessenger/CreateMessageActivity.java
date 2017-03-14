e1d6f4704e090069a65bf0db513fdfcb1e0496b0
David Griffiths <dogriffiths@gmail.com>
2017-03-14 13:17:04 +0000

    feat: Create blank app for chapter 3

---
+++ chapter3/MyMessenger/app/src/main/java/com/hfad/mymessenger/CreateMessageActivity.java
@@ -0,0 +1,13 @@
+package com.hfad.mymessenger;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CreateMessageActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_message);
+    }
+}
