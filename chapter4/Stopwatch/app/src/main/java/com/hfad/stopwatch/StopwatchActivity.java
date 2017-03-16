3225b30891b0a51aa8dfc7c6e4c4bb0c5c346bfc
David Griffiths <dogriffiths@gmail.com>
2017-03-16 12:22:36 +0000

    chore: Build stopwatch app in chapter 4

---
+++ chapter4/Stopwatch/app/src/main/java/com/hfad/stopwatch/StopwatchActivity.java
@@ -0,0 +1,13 @@
+package com.hfad.stopwatch;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class StopwatchActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stopwatch);
+    }
+}
