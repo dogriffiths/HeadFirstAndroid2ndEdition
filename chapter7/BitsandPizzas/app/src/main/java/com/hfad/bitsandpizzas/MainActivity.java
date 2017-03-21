a173710e205de4555e092abfc4d146534fc2985c
David Griffiths <dogriffiths@gmail.com>
2017-03-21 13:23:53 +0000

    feat: Switch app to use Toolbar

--- chapter7/BitsandPizzas/app/src/main/java/com/hfad/bitsandpizzas/MainActivity.java
+++ chapter7/BitsandPizzas/app/src/main/java/com/hfad/bitsandpizzas/MainActivity.java
@@ -2,6 +2,7 @@ package com.hfad.bitsandpizzas;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 }
