6dc3f7cdd2a31158efb0b0ef1027eda4e9d9b763
David Griffiths <dogriffiths@gmail.com>
2017-04-28 13:11:59 +0100

    fix: Correct id and add click callback

--- chapter02/BeerAdviser/app/src/main/java/com/hfad/beeradviser/FindBeerActivity.java
+++ chapter02/BeerAdviser/app/src/main/java/com/hfad/beeradviser/FindBeerActivity.java
@@ -2,6 +2,9 @@ package com.hfad.beeradviser;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class FindBeerActivity extends Activity {
 
@@ -9,5 +12,20 @@ public class FindBeerActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_beer);
+    }
+
+    //Call when the button gets clicked
+    public void onClickFindBeer(View view) {
+        //Get a reference to the TextView
+        TextView brands = (TextView) findViewById(R.id.brands);
+
+        //Get a reference to the Spinner
+        Spinner color = (Spinner) findViewById(R.id.color);
+
+        //Get the selected item in the Spinner
+        String beerType = String.valueOf(color.getSelectedItem());
+
+        //Display the selected item
+        brands.setText(beerType);
     }
 }
