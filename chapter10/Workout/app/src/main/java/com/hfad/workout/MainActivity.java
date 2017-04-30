ae3e251119a7d96cd704b7167ad4764ebd533e70
David Griffiths <dogriffiths@gmail.com>
2017-04-30 14:50:52 +0100

    feat: Display details on a tablet

--- chapter10/Workout/app/src/main/java/com/hfad/workout/MainActivity.java
+++ chapter10/Workout/app/src/main/java/com/hfad/workout/MainActivity.java
@@ -1,8 +1,9 @@
 package com.hfad.workout;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity implements WorkoutListFragment.Listener {
@@ -15,8 +16,20 @@ public class MainActivity extends AppCompatActivity implements WorkoutListFragme
 
     @Override
     public void itemClicked(long id) {
-        Intent intent = new Intent(this, DetailActivity.class);
-        intent.putExtra(DetailActivity.EXTRA_WORKOUT_ID, (int)id);
-        startActivity(intent);
+        View fragmentContainer = findViewById(R.id.fragment_container);
+        if (fragmentContainer != null) {
+            WorkoutDetailFragment details = new WorkoutDetailFragment();
+            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            details.setWorkout(id);
+            ft.replace(R.id.fragment_container, details);
+            ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+            ft.addToBackStack(null);
+            ft.commit();
+        }
+        else {
+            Intent intent = new Intent(this, DetailActivity.class);
+            intent.putExtra(DetailActivity.EXTRA_WORKOUT_ID, (int)id);
+            startActivity(intent);
+        }
     }
 }
