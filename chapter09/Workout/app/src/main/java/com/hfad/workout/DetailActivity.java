2addbeaf937fc95bad6432521554ad734219b6f9
David Griffiths <dogriffiths@gmail.com>
2017-04-30 14:14:18 +0100

    feat: Show workout detail in fragment

--- chapter09/Workout/app/src/main/java/com/hfad/workout/DetailActivity.java
+++ chapter09/Workout/app/src/main/java/com/hfad/workout/DetailActivity.java
@@ -9,5 +9,8 @@ public class DetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+        WorkoutDetailFragment frag = (WorkoutDetailFragment)
+                getSupportFragmentManager().findFragmentById(R.id.detail_frag);
+        frag.setWorkout(1);
     }
 }
--- chapter09/Workout/app/src/main/java/com/hfad/workout/WorkoutDetailFragment.java
+++ chapter09/Workout/app/src/main/java/com/hfad/workout/WorkoutDetailFragment.java
@@ -6,12 +6,14 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class WorkoutDetailFragment extends Fragment {
+    private long workoutId;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -19,4 +21,20 @@ public class WorkoutDetailFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_workout_detail, container, false);
     }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+        View view = getView();
+        if (view != null) {
+            TextView title = (TextView) view.findViewById(R.id.textTitle);
+            Workout workout = Workout.workouts[(int) workoutId];
+            title.setText(workout.getName());
+            TextView description = (TextView) view.findViewById(R.id.textDescription);
+            description.setText(workout.getDescription());
+        }
+    }
+
+    public void setWorkout(long id) {
+        this.workoutId = id;
+    }
 }
