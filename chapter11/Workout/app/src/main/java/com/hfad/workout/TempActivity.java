6cc541094388b32e18cc2ae5e3808295d22483a1
David Griffiths <dogriffiths@gmail.com>
2017-04-30 16:01:54 +0100

    fix: Fix rotation bug in stopwatch fragment

--- chapter11/Workout/app/src/main/java/com/hfad/workout/TempActivity.java
+++ chapter11/Workout/app/src/main/java/com/hfad/workout/TempActivity.java
@@ -1,5 +1,6 @@
 package com.hfad.workout;
 
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,13 @@ public class TempActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_temp);
+        if (savedInstanceState == null) {
+            StopwatchFragment stopwatch = new StopwatchFragment();
+            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            ft.add(R.id.stopwatch_container, stopwatch);
+            ft.addToBackStack(null);
+            ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+            ft.commit();
+        }
     }
 }
