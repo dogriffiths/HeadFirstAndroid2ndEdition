8650060fa2007714c7c6d6d28705c072d96a3182
David Griffiths <dogriffiths@gmail.com>
2017-04-30 09:32:50 +0100

    chore: Add second activity

---
+++ chapter03/MyMessenger/app/src/main/java/com/hfad/mymessenger/ReceiveMessageActivity.java
@@ -0,0 +1,13 @@
+package com.hfad.mymessenger;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ReceiveMessageActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_receive_message);
+    }
+}
