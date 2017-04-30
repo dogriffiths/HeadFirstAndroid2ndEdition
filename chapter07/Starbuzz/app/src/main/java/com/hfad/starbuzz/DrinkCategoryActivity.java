aef13d8d5044735d2523228ecee9c9b4bad251ae
David Griffiths <dogriffiths@gmail.com>
2017-04-30 11:43:14 +0100

    feat: Add coffee detail activity

--- chapter07/Starbuzz/app/src/main/java/com/hfad/starbuzz/DrinkCategoryActivity.java
+++ chapter07/Starbuzz/app/src/main/java/com/hfad/starbuzz/DrinkCategoryActivity.java
@@ -1,7 +1,10 @@
 package com.hfad.starbuzz;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -15,5 +18,24 @@ public class DrinkCategoryActivity extends Activity {
                 this, android.R.layout.simple_list_item_1, Drink.drinks);
         ListView listDrinks = (ListView) findViewById(R.id.list_drinks);
         listDrinks.setAdapter(listAdapter);
+
+        //Create the listener
+        AdapterView.OnItemClickListener itemClickListener =
+                new AdapterView.OnItemClickListener(){
+                    @Override
+                    public void onItemClick(AdapterView<?> listDrinks,
+                                            View itemView,
+                                            int position,
+                                            long id) {
+                        //Pass the drink the user clicks on to DrinkActivity
+                        Intent intent = new Intent(DrinkCategoryActivity.this,
+                                DrinkActivity.class);
+                        intent.putExtra(DrinkActivity.EXTRA_DRINKID, (int) id);
+                        startActivity(intent);
+                    }
+                };
+
+        //Assign the listener to the list view
+        listDrinks.setOnItemClickListener(itemClickListener);
     }
 }
