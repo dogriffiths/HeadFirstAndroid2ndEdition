7f390e5ef510e76ab4b7c195b8c0f7b791ac9dda
David Griffiths <dogriffiths@gmail.com>
2017-04-30 10:22:26 +0100

    feat: Switch to use pause/resume

--- chapter04/Stopwatch/app/src/main/java/com/hfad/stopwatch/StopwatchActivity.java
+++ chapter04/Stopwatch/app/src/main/java/com/hfad/stopwatch/StopwatchActivity.java
@@ -33,18 +33,18 @@ public class StopwatchActivity extends Activity {
     }
 
     @Override
-    protected void onStop() {
-        super.onStop();
-        wasRunning = running;
-        running = false;
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
+    protected void onResume() {
+        super.onResume();
         if (wasRunning) {
             running = true;
         }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        wasRunning = running;
+        running = false;
     }
 
     //Start the stopwatch running when the Start button is clicked.
