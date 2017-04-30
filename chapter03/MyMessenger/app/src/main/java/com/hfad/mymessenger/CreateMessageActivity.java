fe65dab3870eef45ef694c81065821c0afb067d0
David Griffiths <dogriffiths@gmail.com>
2017-04-30 09:39:49 +0100

    feat: Second message displays message from first

--- chapter03/MyMessenger/app/src/main/java/com/hfad/mymessenger/CreateMessageActivity.java
+++ chapter03/MyMessenger/app/src/main/java/com/hfad/mymessenger/CreateMessageActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class CreateMessageActivity extends Activity {
 
@@ -15,7 +16,10 @@ public class CreateMessageActivity extends Activity {
 
     //Call onSendMessage() when the button is clicked
     public void onSendMessage(View view) {
+        EditText messageView = (EditText)findViewById(R.id.message);
+        String messageText = messageView.getText().toString();
         Intent intent = new Intent(this, ReceiveMessageActivity.class);
+        intent.putExtra(ReceiveMessageActivity.EXTRA_MESSAGE, messageText);
         startActivity(intent);
     }
 }
