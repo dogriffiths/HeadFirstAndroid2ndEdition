5b5587e11f9fc2e28e841f594c4e840108d6e09a
David Griffiths <dogriffiths@gmail.com>
2017-04-30 11:26:31 +0100

    feat: Add connection between drinks option and list of drinks

---
+++ chapter07/Starbuzz/app/src/main/java/com/hfad/starbuzz/DrinkCategoryActivity.java
@@ -0,0 +1,19 @@
+package com.hfad.starbuzz;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class DrinkCategoryActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_drink_category);
+        ArrayAdapter<Drink> listAdapter = new ArrayAdapter<>(
+                this, android.R.layout.simple_list_item_1, Drink.drinks);
+        ListView listDrinks = (ListView) findViewById(R.id.list_drinks);
+        listDrinks.setAdapter(listAdapter);
+    }
+}
