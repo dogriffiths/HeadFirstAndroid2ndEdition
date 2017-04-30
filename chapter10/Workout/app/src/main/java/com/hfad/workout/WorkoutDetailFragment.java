11af9557c0a7e8108afa4720826ed50fa0dbc735
David Griffiths <dogriffiths@gmail.com>
2017-04-30 14:54:03 +0100

    feat: Tablets survive configuration change

--- chapter10/Workout/app/src/main/java/com/hfad/workout/WorkoutDetailFragment.java
+++ chapter10/Workout/app/src/main/java/com/hfad/workout/WorkoutDetailFragment.java
@@ -16,6 +16,14 @@ public class WorkoutDetailFragment extends Fragment {
     private long workoutId;
 
     @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        if (savedInstanceState != null) {
+            workoutId = savedInstanceState.getLong("workoutId");
+        }
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_workout_detail, container, false);
@@ -32,6 +40,11 @@ public class WorkoutDetailFragment extends Fragment {
             TextView description = (TextView) view.findViewById(R.id.textDescription);
             description.setText(workout.getDescription());
         }
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        savedInstanceState.putLong("workoutId", workoutId);
     }
 
     public void setWorkout(long id) {
