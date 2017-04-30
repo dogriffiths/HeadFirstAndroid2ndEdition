2ed35e1d20aec72a441e5c834d3dcc162c5f8e3e
David Griffiths <dogriffiths@gmail.com>
2017-04-30 09:25:42 +0100

    feat: Update layout and add button method

--- chapter03/MyMessenger/app/src/main/java/com/hfad/mymessenger/CreateMessageActivity.java
+++ chapter03/MyMessenger/app/src/main/java/com/hfad/mymessenger/CreateMessageActivity.java
@@ -2,6 +2,7 @@ package com.hfad.mymessenger;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 
 public class CreateMessageActivity extends Activity {
 
@@ -9,5 +10,9 @@ public class CreateMessageActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_message);
+    }
+
+    //Call onSendMessage() when the button is clicked
+    public void onSendMessage(View view) {
     }
 }
