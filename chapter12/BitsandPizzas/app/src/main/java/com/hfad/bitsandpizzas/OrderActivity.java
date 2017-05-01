61fdf394fe7be0e0fecdb8cadb1e7952310d228a
David Griffiths <dogriffiths@gmail.com>
2017-05-01 09:33:06 +0100

    chore: Copy BitsAndPizzas over to chapter 12

---
+++ chapter12/BitsandPizzas/app/src/main/java/com/hfad/bitsandpizzas/OrderActivity.java
@@ -0,0 +1,19 @@
+package com.hfad.bitsandpizzas;
+
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+public class OrderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+}
