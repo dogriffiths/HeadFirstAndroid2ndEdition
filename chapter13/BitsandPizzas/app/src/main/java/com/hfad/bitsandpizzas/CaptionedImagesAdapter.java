468465b2667f408d90b50fad2403fc21d396ff61
David Griffiths <dogriffiths@gmail.com>
2017-05-04 13:10:32 +0100

    feat: Add cardview for pizzas

---
+++ chapter13/BitsandPizzas/app/src/main/java/com/hfad/bitsandpizzas/CaptionedImagesAdapter.java
@@ -0,0 +1,58 @@
+package com.hfad.bitsandpizzas;
+
+import android.graphics.drawable.Drawable;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+/**
+ * Created by davidg on 04/05/2017.
+ */
+
+class CaptionedImagesAdapter extends RecyclerView.Adapter<CaptionedImagesAdapter.ViewHolder> {
+
+    private String[] captions;
+    private int[] imageIds;
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        private CardView cardView;
+
+        public ViewHolder(CardView v) {
+            super(v);
+            cardView = v;
+        }
+    }
+
+    public CaptionedImagesAdapter(String[] captions, int[] imageIds) {
+        this.captions = captions;
+        this.imageIds = imageIds;
+    }
+
+    @Override
+    public int getItemCount() {
+        return captions.length;
+    }
+
+    @Override
+    public CaptionedImagesAdapter.ViewHolder onCreateViewHolder(
+            ViewGroup parent, int viewType){
+        CardView cv = (CardView) LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.card_captioned_image, parent, false);
+        return new ViewHolder(cv);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position){
+        CardView cardView = holder.cardView;
+        ImageView imageView = (ImageView)cardView.findViewById(R.id.info_image);
+        Drawable drawable = ContextCompat.getDrawable(cardView.getContext(), imageIds[position]);
+        imageView.setImageDrawable(drawable);
+        imageView.setContentDescription(captions[position]);
+        TextView textView = (TextView)cardView.findViewById(R.id.info_text);
+        textView.setText(captions[position]);
+    }
+}
--- chapter13/BitsandPizzas/app/src/main/java/com/hfad/bitsandpizzas/PizzaFragment.java
+++ chapter13/BitsandPizzas/app/src/main/java/com/hfad/bitsandpizzas/PizzaFragment.java
@@ -3,24 +3,40 @@ package com.hfad.bitsandpizzas;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.ListFragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class PizzaFragment extends ListFragment {
+public class PizzaFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(
-                inflater.getContext(), android.R.layout.simple_list_item_1,
-                getResources().getStringArray(R.array.pizzas));
-        setListAdapter(adapter);
-        return super.onCreateView(inflater, container, savedInstanceState);
+        RecyclerView pizzaRecycler = (RecyclerView)inflater.inflate(
+                R.layout.fragment_pizza, container, false);
+
+        String[] pizzaNames = new String[Pizza.pizzas.length];
+
+        for (int i = 0; i < pizzaNames.length; i++) {
+            pizzaNames[i] = Pizza.pizzas[i].getName();
+        }
+
+        int[] pizzaImages = new int[Pizza.pizzas.length];
+
+        for (int i = 0; i < pizzaImages.length; i++) {
+            pizzaImages[i] = Pizza.pizzas[i].getImageResourceId();
+        }
+
+        CaptionedImagesAdapter adapter = new CaptionedImagesAdapter(pizzaNames, pizzaImages);
+        pizzaRecycler.setAdapter(adapter);
+        GridLayoutManager layoutManager = new GridLayoutManager(getActivity(), 2);
+        pizzaRecycler.setLayoutManager(layoutManager);
+
+        return pizzaRecycler;
     }
 }
