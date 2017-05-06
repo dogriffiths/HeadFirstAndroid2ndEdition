7e168cb5a9b284e6913f4cc57e129739560722fc
David Griffiths <dogriffiths@gmail.com>
2017-05-06 19:39:00 +0100

    feat: Replace random odometer with real one

--- chapter18/Odometer/app/src/main/java/com/hfad/odometer/OdometerService.java
+++ chapter18/Odometer/app/src/main/java/com/hfad/odometer/OdometerService.java
@@ -1,16 +1,29 @@
 package com.hfad.odometer;
 
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Binder;
+import android.os.Bundle;
 import android.os.IBinder;
+import android.support.v4.content.ContextCompat;
 
 import java.util.Random;
 
 public class OdometerService extends Service {
 
     private final IBinder binder = new OdometerBinder();
-    private final Random random = new Random();
+    private LocationListener listener;
+    private LocationManager locManager;
+    private static double distanceInMeters;
+    private static Location lastLocation = null;
+    public static final String PERMISSION_STRING
+            = android.Manifest.permission.ACCESS_FINE_LOCATION;
 
     public class OdometerBinder extends Binder {
         OdometerService getOdometer() {
@@ -19,11 +32,59 @@ public class OdometerService extends Service {
     }
 
     @Override
+    public void onCreate() {
+        super.onCreate();
+        listener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                if (lastLocation == null) {
+                    lastLocation = location; }
+                distanceInMeters += location.distanceTo(lastLocation);
+                lastLocation = location;
+            }
+
+            @Override
+            public void onProviderDisabled(String arg0) {
+            }
+
+            @Override
+            public void onProviderEnabled(String arg0) {
+            }
+
+            @Override
+            public void onStatusChanged(String arg0, int arg1, Bundle bundle) {
+            }
+        };
+
+        locManager = (LocationManager) getSystemService (Context.LOCATION_SERVICE);
+        if (ContextCompat.checkSelfPermission(this, PERMISSION_STRING)
+                == PackageManager.PERMISSION_GRANTED) {
+            String provider = locManager.getBestProvider(new Criteria(), true);
+            if (provider != null) {
+                locManager.requestLocationUpdates(provider, 1000, 1, listener);
+            }
+        }
+    }
+
+    @Override
     public IBinder onBind(Intent intent) {
         return binder;
     }
 
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (locManager != null && listener != null) {
+            if (ContextCompat.checkSelfPermission(this, PERMISSION_STRING)
+                    == PackageManager.PERMISSION_GRANTED) {
+                locManager.removeUpdates(listener);
+            }
+            locManager = null;
+            listener = null;
+        }
+    }
+
     public double getDistance() {
-        return random.nextDouble();
+        return this.distanceInMeters / 1609.344;
     }
 }
