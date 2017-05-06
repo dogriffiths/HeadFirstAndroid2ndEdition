7fa59108600951849f210c77fdd86f3d0c880526
David Griffiths <dogriffiths@gmail.com>
2017-05-06 18:02:06 +0100

    feat: Display punch-line in the log

---
+++ chapter18/Joke/app/src/main/java/com/hfad/joke/DelayedMessageService.java
@@ -0,0 +1,31 @@
+package com.hfad.joke;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.util.Log;
+
+public class DelayedMessageService extends IntentService {
+
+    public static final String EXTRA_MESSAGE = "message";
+
+    public DelayedMessageService() {
+        super("DelayedMessageService");
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        synchronized (this) {
+            try {
+                wait(10000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        String text = intent.getStringExtra(EXTRA_MESSAGE);
+        showText(text);
+    }
+
+    private void showText(final String text) {
+        Log.v("DelayedMessageService", "The message is: " + text);
+    }
+}
