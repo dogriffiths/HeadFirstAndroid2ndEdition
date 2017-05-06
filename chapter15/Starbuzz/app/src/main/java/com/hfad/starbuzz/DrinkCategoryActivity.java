a0e58f7586a33bc55f9424e9d71cfb62dbe7b4f0
David Griffiths <dogriffiths@gmail.com>
2017-05-06 16:22:21 +0100

    chore: Copy Starbuzz from a previous chapter

---
+++ chapter15/Starbuzz/app/src/main/java/com/hfad/starbuzz/DrinkCategoryActivity.java
@@ -0,0 +1,41 @@
+package com.hfad.starbuzz;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class DrinkCategoryActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_drink_category);
+        ArrayAdapter<Drink> listAdapter = new ArrayAdapter<>(
+                this, android.R.layout.simple_list_item_1, Drink.drinks);
+        ListView listDrinks = (ListView) findViewById(R.id.list_drinks);
+        listDrinks.setAdapter(listAdapter);
+
+        //Create the listener
+        AdapterView.OnItemClickListener itemClickListener =
+                new AdapterView.OnItemClickListener(){
+                    @Override
+                    public void onItemClick(AdapterView<?> listDrinks,
+                                            View itemView,
+                                            int position,
+                                            long id) {
+                        //Pass the drink the user clicks on to DrinkActivity
+                        Intent intent = new Intent(DrinkCategoryActivity.this,
+                                DrinkActivity.class);
+                        intent.putExtra(DrinkActivity.EXTRA_DRINKID, (int) id);
+                        startActivity(intent);
+                    }
+                };
+
+        //Assign the listener to the list view
+        listDrinks.setOnItemClickListener(itemClickListener);
+    }
+}
