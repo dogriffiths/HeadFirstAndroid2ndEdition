68eb5469ab2baef3dfc5d827927da1f2d3e6a176
David Griffiths <dogriffiths@gmail.com>
2017-05-06 18:22:48 +0100

    feat: Swap log message with notification

--- chapter18/Joke/app/src/main/java/com/hfad/joke/DelayedMessageService.java
+++ chapter18/Joke/app/src/main/java/com/hfad/joke/DelayedMessageService.java
@@ -1,12 +1,15 @@
 package com.hfad.joke;
 
 import android.app.IntentService;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Intent;
-import android.util.Log;
+import android.support.v4.app.NotificationCompat;
 
 public class DelayedMessageService extends IntentService {
 
     public static final String EXTRA_MESSAGE = "message";
+    public static final int NOTIFICATION_ID = 5453;
 
     public DelayedMessageService() {
         super("DelayedMessageService");
@@ -26,6 +29,26 @@ public class DelayedMessageService extends IntentService {
     }
 
     private void showText(final String text) {
-        Log.v("DelayedMessageService", "The message is: " + text);
+        //Create a notification builder
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this)
+                .setSmallIcon(android.R.drawable.sym_def_app_icon)
+                .setContentTitle(getString(R.string.question))
+                .setContentText(text)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setVibrate(new long[] {0, 1000})
+                .setAutoCancel(true);
+
+        //Create an action
+        Intent actionIntent = new Intent(this, MainActivity.class);
+        PendingIntent actionPendingIntent = PendingIntent.getActivity(
+                this,
+                0,
+                actionIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+        builder.setContentIntent(actionPendingIntent);
+
+        //Issue the notification
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        notificationManager.notify(NOTIFICATION_ID, builder.build());
     }
 }
